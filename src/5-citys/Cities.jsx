c9d143a8410b8b7ddec28cc638a1bdc80e82953b
ibrahim <himaovic1@gamil.com>
2024-01-21 15:44:36 +0200

    adding img

--- src/5-citys/Cities.jsx
+++ src/5-citys/Cities.jsx
@@ -6,6 +6,7 @@ const imags = [{title:"russai", imgpath:"russia.jpg" ,title2: "."},
 {title:"china",title2:"lorem", imgpath:"pexels-johannes-plenio-1119972.jpg"},
 {title:"france", imgpath:"francs2.jpg", title2: "lorem"},
 {title:"england", imgpath:"england.jpg", title2: "lorem"},
+{title:"egypt", imgpath:"egypt.jpg", title2: "lorem"},
 ]
 const Cities = () => {
   const [arr, setarr] = useState(imags)
@@ -18,7 +19,6 @@ const Cities = () => {
               <div key={e.title} className='div1'>
               <img src= {e.imgpath} />
               <h2> {e.title} </h2>
-             
             </div>
             )
           }
